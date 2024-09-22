a7f141d093816be91bb01d72ff2b37f4f5b0c248
jedelmondes <jenniferdesah49@gmail.com>
2024-09-22 17:46:51 -0300

    simulacao de bug

--- IMC-1.py
+++ IMC-1.py
@@ -5,9 +5,9 @@ def calcular_imc(peso, altura):
 def classificar_imc(imc):
     if imc < 18.5:
         return "Abaixo do peso"
-    elif 18.5 <= imc < 24.9:
+    else 18.5 <= imc < 24.9:
         return "Peso normal"
-    elif 25 <= imc < 29.9:
+    else 25 <= imc < 29.9:
         return "Sobrepeso"
     else:
         return "Obesidade"
